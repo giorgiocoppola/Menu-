37cd9b3cc73e475ccf9bd5754e2867b6c2e6dd8d
giorgiocoppola <giorgiocoppola12@gmail.com>
2021-11-24 21:49:29 +0100

    Utilizzo degli use state di incremento e di decremento del bottoni

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,26 +7,32 @@ import React,{useState} from 'react'
 
 function Menu() {
   const [count,setcount] = useState(1)
+  const [count2,setcount2] = useState(1)
+  const [count3,setcount3] = useState(1)
          return (
         <div className="container">
          <h1>Cosa vorresti ordinare?</h1>
          <div className ="testo1"> <img src={pesce} width="300px" height="230px"></img>
          <div className="descrizione">Fritto di pesce</div>
          <div className="prezzo">30 €</div>
-         <button onClick={() => setcount(count + 1)}>Ordina</button>
+         <button onClick={() => setcount(count + 1)}>Aggiungi</button>
          <p>{count}</p>
+         <button onClick={() => setcount(count - 1)}>Togli</button>
          </div>
          <div className ="testo2"> <img src={pesce2} width="300px" height="230px"></img>
          <div className="descrizione">Zuppa di pesce</div>
          <div className="prezzo">55 €</div>
-         <button onClick={() => setcount(count + 1)}>Ordina</button>
-         <p>{count}</p>
+         <button onClick={() => setcount2(count2 + 1)}>Aggiungi</button>
+         <p>{count2}</p>
+         <button onClick={() => setcount2(count2 - 1)}>Togli</button>
          </div>
          <div className ="testo3"><img src={pesce3} width="300px" height="230px"></img>
          <div className="descrizione">Fritto di paranza</div>
          <div className="prezzo">56 €</div>
-         <button onClick={() => setcount(count + 1)}>Ordina</button>
-         <p>{count}</p>
+         <button onClick={() => setcount3(count3 + 1)}>Aggiungi</button>
+         <p>{count3}</p>
+         <button onClick={() => setcount3(count3 - 1)}>Togli</button>
+     
          </div>
        
          </div>  
