844936c4ea0c8e548ef9e2c731c8af0431893195
giorgiocoppola <giorgiocoppola12@gmail.com>
2021-11-30 20:44:34 +0100

    Modifica dei prezzi dei pasti

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,33 +6,32 @@ import React,{useState} from 'react'
 
 
 function Menu() {
-  const [count,setcount] = useState(1)
-  const [count2,setcount2] = useState(1)
-  const [count3,setcount3] = useState(1)
+  const [count,setcount] = useState(0)
+  const [count2,setcount2] = useState(0)
+  const [count3,setcount3] = useState(0)
          return (
         <div className="container">
          <h1>Cosa vorresti ordinare?</h1>
          <div className ="testo1"> <img src={pesce} width="300px" height="230px"></img>
          <div className="descrizione">Fritto di pesce</div>
-         <div className="prezzo">30 €</div>
+         <div className="prezzo">15 €</div>
          <button onClick={() => setcount(count + 1)}>Aggiungi</button>
-         <p>{count}</p>
-         <button onClick={() => setcount(count - 1)}>Togli</button>
+         <button onClick={() =>{ count === 0 ? setcount(0) : setcount(count - 1)}}>Togli</button>
+         <p>{count}</p>  
          </div>
          <div className ="testo2"> <img src={pesce2} width="300px" height="230px"></img>
          <div className="descrizione">Zuppa di pesce</div>
-         <div className="prezzo">55 €</div>
+         <div className="prezzo">13 €</div>
          <button onClick={() => setcount2(count2 + 1)}>Aggiungi</button>
+         <button onClick={() => { count2 === 0 ? setcount2(0) : setcount2(count2 - 1)}}>Togli</button>
          <p>{count2}</p>
-         <button onClick={() => setcount2(count2 - 1)}>Togli</button>
          </div>
          <div className ="testo3"><img src={pesce3} width="300px" height="230px"></img>
          <div className="descrizione">Fritto di paranza</div>
-         <div className="prezzo">56 €</div>
+         <div className="prezzo">16 €</div>
          <button onClick={() => setcount3(count3 + 1)}>Aggiungi</button>
+         <button onClick={() => { count3 === 0 ? setcount3(0) : setcount3(count3 - 1)}}>Togli</button>
          <p>{count3}</p>
-         <button onClick={() => setcount3(count3 - 1)}>Togli</button>
-     
          </div>
        
          </div>  
